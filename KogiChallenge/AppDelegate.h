cdc1836699a33b619afbf6d9fdefd4979aca0996
Ruben Bueno <administrator@RBmac.local>
2013-02-25 18:20:02 -0500

    Initial Commit

---
+++ KogiChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KogiChallenge
+//
+//  Created by Ruben Bueno on 25/02/2013.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ KogiChallenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KogiChallenge
+//
+//  Created by Ruben Bueno on 25/02/2013.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
